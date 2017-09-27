4f7315abdcaa9cb0aaf7e21daf7f5d835476301d
Willi Rath <wrath@geomar.de>
2017-09-27 19:55:06 +0200

    Add setup script for easier installation

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""Setup gnomon."""
+
+from setuptools import setup
+
+setup(name='rapidmoc',
+      description='Calculates observational-style decomposition of AMOC using '
+                  'output from an ocean general circulation model.',
+      packages=['rapidmoc'],
+      package_dir={'rapidmoc': 'rapidmoc'},
+      package_data={'rapidmoc': ['etc/*']},
+      install_requires=['setuptools', 'netCDF4', 'numpy',  'matplotlib',
+                        'scipy'],
+      entry_points={
+          'console_scripts':
+          ['run_rapidmoc.py = rapidmoc.rapidmoc:main']},
+      zip_safe=False)
